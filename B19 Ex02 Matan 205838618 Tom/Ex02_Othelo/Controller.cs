7505a4c4ffe877edd48a246bb866bfcbf0e8041a
TomLezmy <49162863+TomLezmy@users.noreply.github.com>
2019-05-09 17:16:03 +0300

    added Turn Functions
    GetTurn
    PlayTurn
    CalculateMoves

--- B19 Ex02 Matan 205838618 Tom/Ex02_Othelo/Controller.cs
+++ B19 Ex02 Matan 205838618 Tom/Ex02_Othelo/Controller.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Ex02_Othelo
 {
@@ -14,9 +15,26 @@ namespace Ex02_Othelo
             return UserUI.GetInputFromUser();
         }
 
-        public static void DrawBorad(int i_BoardSize, int[,] i_Pieces, int i_PlayerTurn)
+        public static void DrawBoard(int i_BoardSize, int[,] i_Pieces, int i_PlayerTurn)
         {
             UserUI.DrawBoard(i_BoardSize, i_Pieces, i_PlayerTurn);
+        }
+
+        public static void GetTurn(out int o_RowIndex, out int o_ColIndex, List<string> i_PossibleMoves)
+        {
+            string inputFromUserStr = "";
+
+            inputFromUserStr = UserUI.GetInputFromUser();
+            inputFromUserStr = inputFromUserStr.ToUpper();
+            while (!i_PossibleMoves.Contains(inputFromUserStr))
+            {
+                UserUI.ShowMessage("Illegal move, try again");
+                inputFromUserStr = UserUI.GetInputFromUser();
+                inputFromUserStr = inputFromUserStr.ToUpper();
+            }
+
+            o_ColIndex = (int)(inputFromUserStr[0] - 'A' + 1) - 1;
+            o_RowIndex = int.Parse(inputFromUserStr[1].ToString()) - 1;
         }
     }
 }
--- B19 Ex02 Matan 205838618 Tom/Ex02_Othelo/Game.cs
+++ B19 Ex02 Matan 205838618 Tom/Ex02_Othelo/Game.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Ex02_Othelo
 {
@@ -23,6 +24,42 @@ Please Write your Name and then press Enter:"
             m_FirstUser = Controller.GetInputFromUser();
 
  
+        }
+
+        public void PlayTurn()
+        {
+            int rowIndex, colIndex;
+
+            Controller.GetTurn(out rowIndex, out colIndex, CalculateMoves());
+            m_Board[rowIndex, colIndex] = m_PlayerTurn + 1;
+            m_PlayerTurn = (m_PlayerTurn + 1) % 2;
+            Controller.DrawBoard(m_BoardSize, m_Board, m_PlayerTurn);
+            if (m_PlayerTurn == 0)
+            {
+                m_FirstUserScore++;
+            }
+            else
+            {
+                m_SecondUserScore++;
+            }
+        }
+
+        public List<string> CalculateMoves()
+        {
+            List<string> possibleMoves = new List<string>();
+
+            for (int i = 0; i < m_BoardSize; i++)
+            {
+                for (int j = 0; j < m_BoardSize; j++)
+                {
+                    if (m_Board[i, j] == 0)
+                    {
+                        possibleMoves.Add((char)('A' + j) + (i + 1).ToString());
+                    }
+                }
+            }
+
+            return possibleMoves;
         }
     }
 }
