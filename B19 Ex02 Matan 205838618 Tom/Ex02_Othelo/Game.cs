9a2a183668bed0480bb850059ab50affb1ddce87
TomLezmy <49162863+TomLezmy@users.noreply.github.com>
2019-05-09 16:35:35 +0300

    added data members

--- B19 Ex02 Matan 205838618 Tom/Ex02_Othelo/Game.cs
+++ B19 Ex02 Matan 205838618 Tom/Ex02_Othelo/Game.cs
@@ -5,6 +5,14 @@ namespace Ex02_Othelo
     class Game
     {
         private string m_FirstUser;
+        private string m_SecondUser;
+        private int m_FirstUserScore;
+        private int m_SecondUserScore;
+        private int m_BoardSize;
+        private bool m_IsTwoPlayer;
+        private int[,] m_Board;
+        private int m_PlayerTurn;
+        private bool m_GameOver;
 
         public Game()
         {
